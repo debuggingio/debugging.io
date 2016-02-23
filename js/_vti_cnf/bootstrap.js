6d30ba45409b2374a2fee5a29a41b95e5f4ffc54
rohithkrajan <rohithkrajan@gmail.com>
2016-02-23 10:45:44 +0530

    initial commit

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 May 2014 13:31:39 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|FAREAST\\rohitra
+vti_modifiedby:SR|FAREAST\\rohitra
+vti_timecreated:TR|24 May 2014 13:31:39 -0000
+vti_cacheddtm:TX|24 May 2014 13:31:39 -0000
+vti_filesize:IR|55258
+vti_backlinkinfo:VX|
---
+++ plugin/leap/_vti_cnf/leap.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|25 Mar 2014 20:36:09 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|FAREAST\\rohitra
+vti_modifiedby:SR|FAREAST\\rohitra
+vti_timecreated:TR|25 Mar 2014 20:36:09 -0000
+vti_cacheddtm:TX|25 Mar 2014 20:36:09 -0000
+vti_filesize:IR|84420
+vti_backlinkinfo:VX|
