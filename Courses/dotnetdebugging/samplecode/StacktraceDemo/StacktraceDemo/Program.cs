664c19f738e258cbe3dfa2185e524fb359edbad1
rohithkrajan <rohithkrajan@gmail.com>
2016-02-26 16:33:05 +0530

    adding stacktracer demo console source code

---
+++ Courses/dotnetdebugging/samplecode/StacktraceDemo/StacktraceDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacktraceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method1();           
+            
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("In Method 1");
+            System.Threading.Thread.Sleep(20000);
+            Method2();
+        }
+        static void Method2()
+        {
+            Console.WriteLine("In Method 2");
+            System.Threading.Thread.Sleep(20000);
+            Method3();
+        }
+        static void Method3()
+        {
+            Console.WriteLine("In Method 3");
+            System.Threading.Thread.Sleep(20000);
+        }
+    }
+}
